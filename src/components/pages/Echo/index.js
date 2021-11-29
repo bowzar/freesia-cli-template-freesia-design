363d00cbfee03275127208b8a658d94b773a19ec
juyang <juyang@yulintu.com>
2021-11-29 18:25:49 +0800

    1

--- src/components/pages/Echo/index.js
+++ src/components/pages/Echo/index.js
@@ -1,7 +1,13 @@
 import React, { useContext } from 'react';
+import { Application } from "@yulintu/freesia-design";
 
 const Echo = function (props) {
-    console.log("Echo", props);
+
+    const context = useContext(Application.Context);
+
+    console.log("echo props", props);
+    console.log("echo context", context);
+
     return <div className="h100 w100 layout-h center2" style={{
         fontSize: 128,
         color: "#bfbfbf"
