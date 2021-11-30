543985a2dde80aea0006a06dcdd795515907e562
juyang <juyang@yulintu.com>
2021-11-30 21:59:40 +0800

    1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,9 @@ const { createProxyMiddleware: proxy } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(proxy("/account", {
-        "target": "http://192.168.20.10:5552/"
+        target: "http://192.168.20.10:5552"
     }));
-    // app.use(proxy('/test', { target: 'http://localhost:3000' }));
-
+    // app.use(proxy('/api', {
+    //     target: 'http://localhost:8080'
+    // }));
 };
