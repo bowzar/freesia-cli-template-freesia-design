66d91dcbc0a7cabf5a266ee91cdf585b8acca5fd
juyang <juyang@yulintu.com>
2021-12-10 18:21:52 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './setupPublicPath';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Application } from "@yulintu/freesia-design";
@@ -5,8 +6,32 @@ import { Application } from "@yulintu/freesia-design";
 import templates from "./templates";
 import './css/index.less';
 
-ReactDOM.render(
-	<Application global={{}} templates={templates} />,
-	document.getElementById('root')
-);
+function render(props = {}) {
+	let { container } = props;
+	container = container ? container.querySelector('#root') : document.querySelector('#root');
 
+	ReactDOM.render((
+		<Application global={{}} templates={templates} {...props} options={{
+			// svgIconPrefix: "xxxx"
+		}} />
+	), container);
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+	render();
+}
+
+export async function bootstrap() {
+	console.log('microapp bootstrap');
+}
+
+export async function mount(props) {
+	console.log('microapp mount', props);
+	render(props);
+}
+
+export async function unmount(props) {
+	console.log('microapp unmount', props);
+	const { container } = props;
+	ReactDOM.unmountComponentAtNode(container ? container.querySelector('#root') : document.querySelector('#root'));
+}
