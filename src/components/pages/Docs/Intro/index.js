1203ed718ebbbbbf6891c96bb1a8971a23e0028b
juyang <juyang@yulintu.com>
2022-01-13 14:11:05 +0800

    1

--- src/components/pages/Docs/Intro/index.js
+++ src/components/pages/Docs/Intro/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography, Image } from 'antd';
-import { useResource } from "@yulintu/freesia-bootstrap";
+import { resourceUrl } from "@yulintu/freesia-bootstrap";
 import { DocumentContainer, Title } from "@yulintu/freesia-design";
 
 const { Paragraph, Text, Link } = Typography;
@@ -11,7 +11,7 @@ const Intro = function (props) {
         <DocumentContainer>
             <Typography>
                 <Title id="概览">概览</Title>
-                <Image className="noshadow" preview={false} src={useResource("/images/docs/intro.png")}></Image>
+                <Image className="noshadow" preview={false} src={resourceUrl("/images/docs/intro.png")}></Image>
                 <Text type="secondary">图片来自 Ant Design 官网</Text>
                 <Paragraph>
                     这是一个基于鱼鳞图 Freesia Design 的前端工程。
