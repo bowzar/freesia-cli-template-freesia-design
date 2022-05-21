97e9a016b86e97aa85f2e09f82cf4b3607820225
juyang <juyang@yulintu.com>
2022-05-21 21:14:33 +0800

    1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,12 @@
 const { createProxyMiddleware: proxy } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+    // app.use(proxy("/account", {
+    //     target: "http://account-dev.yulintu.com",
+    //     changeOrigin: true,
+    // }));
     app.use(proxy("/account", {
-        target: "http://account-dev.yulintu.com",
+        target: "http://dev.yizhangtu.com:9552",
         changeOrigin: true,
     }));
     // app.use(proxy('/api', {
